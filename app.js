c5f0f618b83005669b85f33e0c0752f44ff6d728
TathyaShah <tathyashah1130@gmail.com>
2024-09-03 11:49:37 +0530

    mostly done now

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 passport.use(new GoogleStrategy({
   clientID: process.env.ID,
   clientSecret: process.env.SECRET,
-  callbackURL: 'http://localhost:3000/auth/google/callback',
+  callbackURL: '/auth/google/callback',
   scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']
 }, async (accessToken, refreshToken, profile, done) => {
   const email = profile.emails[0].value;
