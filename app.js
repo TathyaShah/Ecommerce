80b6de983b8e4d3a81b61358025c4fca99d0262e
TathyaShah <tathyashah1130@gmail.com>
2025-04-28 14:05:42 +0530

    Google login fixed now

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ passport.use(new GoogleStrategy({
   clientID: process.env.ID,
   clientSecret: process.env.SECRET,
   // callbackURL: '/auth/google/callback',
-  callbackURL: 'https://ecommerce-ug71.onrender.com/auth/google/callback',
+  callbackURL: 'https://ecommerce-3fje.onrender.com/auth/google/callback',
   scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']
 }, async (accessToken, refreshToken, profile, done) => {
   const email = profile.emails[0].value;
